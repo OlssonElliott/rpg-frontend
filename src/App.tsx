70af0bb87779b426d92a800aa11b8ce098a85299
Elliott <sendtoelle@hotmail.com>
2025-10-19 12:56:55 +0200

    deploy, med tester

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
 
 import CombatTester from "./components/CombatTester";
+import DungeonTester from "./components/DungeonTester";
 
 function App() {
   return (
     <div className="app-shell">
-      <CombatTester />
+      <header className="app-header">
+        <h1>RPG Testverktyg</h1>
+        <p>
+          Snabbpaneler för att prova både dungeon-sessioner och combat-motorn.
+        </p>
+      </header>
+
+      <div className="tester-grid">
+        <DungeonTester />
+        <CombatTester />
+      </div>
     </div>
   );
 }
